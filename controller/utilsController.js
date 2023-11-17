d8422644b3418a65849810e1f1713391f6c381f3
LamTien2003 <ltt209514@gmail.com>
2023-11-17 22:22:22 +0700

    adding get location

--- controller/utilsController.js
+++ controller/utilsController.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const catchAsync = require('../utils/catchAsync');
 const { sendResponseToClient } = require('../utils/ultils');
 
@@ -7,5 +9,33 @@ exports.getSkills = catchAsync(async (req, res, next) => {
     sendResponseToClient(res, 200, {
         status: 'success',
         data: skills || [],
+    });
+});
+
+exports.getLocation = catchAsync(async (req, res, next) => {
+    const location = await axios.get('https://provinces.open-api.vn/api/?depth=2');
+    sendResponseToClient(res, 200, {
+        status: 'success',
+        data: location.data,
+    });
+});
+
+exports.getDistrict = catchAsync(async (req, res, next) => {
+    const { code } = req.query;
+    const district = await axios.get(`https://provinces.open-api.vn/api/d/${code ? code : ''}`);
+
+    sendResponseToClient(res, 200, {
+        status: 'success',
+        data: district.data,
+    });
+});
+
+exports.getCity = catchAsync(async (req, res, next) => {
+    const { code } = req.query;
+    const district = await axios.get(`https://provinces.open-api.vn/api/p/${code ? code : ''}`);
+
+    sendResponseToClient(res, 200, {
+        status: 'success',
+        data: district.data,
     });
 });
--- routes/utilsRoute.js
+++ routes/utilsRoute.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const utilsController = require('../controller/utilsController');
 
 router.get('/getSkills', utilsController.getSkills);
+router.get('/getLocation', utilsController.getLocation);
+router.get('/getDistrict', utilsController.getDistrict);
+router.get('/getCity', utilsController.getCity);
 
 module.exports = router;
